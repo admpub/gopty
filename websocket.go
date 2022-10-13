4465673d1903c713fee4e12ae5ab647e27b8a7ce
Hank Shen <swh@admpub.com>
2022-10-13 21:58:55 +0800

    update

--- websocket.go
+++ websocket.go
@@ -8,10 +8,10 @@ import (
 	"unicode/utf8"
 
 	"github.com/admpub/gopty/interfaces"
-	"github.com/admpub/websocket"
 )
 
-func PTY2Websocket(ws *websocket.Conn, pty interfaces.Console) {
+// PTY2Websocket pty to websocket
+func PTY2Websocket(ws WebsocketWriter, pty interfaces.Console) {
 	buffer := make([]byte, 1024)
 	var payload, overflow []byte
 	for {
@@ -34,7 +34,7 @@ func PTY2Websocket(ws *websocket.Conn, pty interfaces.Console) {
 		}
 
 		if len(payload) >= 1 {
-			err = ws.WriteMessage(websocket.BinaryMessage, payload[:len(payload)])
+			err = ws.WriteMessage(BinaryMessage, payload[:])
 			if err != nil {
 				fmt.Println("[PTY2Websocket] write to ws error: ", err)
 				return
@@ -46,7 +46,49 @@ func PTY2Websocket(ws *websocket.Conn, pty interfaces.Console) {
 	}
 }
 
-func Websocket2PTY(ws *websocket.Conn, pty interfaces.Console) {
+// WebsocketWriter websocket writer
+type WebsocketWriter interface {
+	WriteMessage(int, []byte) error
+}
+
+// WebsocketReader websocket reader
+type WebsocketReader interface {
+	ReadMessage() (int, []byte, error)
+}
+
+// Websocketer websocket interface
+// github.com/admpub/websocket
+// github.com/gorilla/websocket
+type Websocketer interface {
+	WebsocketWriter
+	WebsocketReader
+}
+
+// The message types are defined in RFC 6455, section 11.8.
+const (
+	// TextMessage denotes a text data message. The text message payload is
+	// interpreted as UTF-8 encoded text data.
+	TextMessage = 1
+
+	// BinaryMessage denotes a binary data message.
+	BinaryMessage = 2
+
+	// CloseMessage denotes a close control message. The optional message
+	// payload contains a numeric code and text. Use the FormatCloseMessage
+	// function to format a close message payload.
+	CloseMessage = 8
+
+	// PingMessage denotes a ping control message. The optional message payload
+	// is UTF-8 encoded text.
+	PingMessage = 9
+
+	// PongMessage denotes a pong control message. The optional message payload
+	// is UTF-8 encoded text.
+	PongMessage = 10
+)
+
+// Websocket2PTY websocket to pty
+func Websocket2PTY(ws WebsocketReader, pty interfaces.Console) {
 	for {
 		mt, message, err := ws.ReadMessage()
 		if mt == -1 || err != nil {
@@ -55,15 +97,21 @@ func Websocket2PTY(ws *websocket.Conn, pty interfaces.Console) {
 		}
 		msg := string(message)
 		if strings.HasPrefix(msg, "<RESIZE>") {
-			size := msg[len("<RESIZE>"):len(msg)]
-			sizeArr := strings.Split(size, ",")
+			size := msg[len("<RESIZE>"):]
+			sizeArr := strings.SplitN(size, ",", 2)
+			if len(sizeArr) != 2 {
+				_, err = pty.Write(message)
+				if err != nil {
+					log.Println("[Websocket2PTY] pty write error: ", err)
+				}
+				continue
+			}
 			rows, _ := strconv.Atoi(sizeArr[0])
 			cols, _ := strconv.Atoi(sizeArr[1])
 			err = pty.SetSize(cols, rows)
 			log.Printf("[Websocket2PTY] pty resize window to %d, %d", cols, rows)
 			if err != nil {
 				log.Println("[Websocket2PTY] pty resize error: ", err)
-				return
 			}
 		} else {
 			_, err = pty.Write(message)
@@ -74,8 +122,9 @@ func Websocket2PTY(ws *websocket.Conn, pty interfaces.Console) {
 	}
 }
 
-func Bash(wsc *websocket.Conn) error {
-	pty, err := New(120, 60)
+// Bash Bash(wsc,120,60)
+func Bash(wsc Websocketer, cols, rows int) error {
+	pty, err := New(cols, rows)
 	if err != nil {
 		return err
 	}
